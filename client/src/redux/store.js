e42d8084602d783fc6985ef420802be283ac6de9
vasudevshetty <vasudeepu2815@gmail.com>
2025-05-27 21:26:03 +0530

    feat: Implement profile update and password change functionality with Redux state management

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import userReducer from "./slices/userSlice";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  user: userReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
