30db1f44ae173094361bb73d2087a931d112fe52
Vasudevshetty <vasudeepu2815@gmail.com>
2025-06-26 09:28:26 +0530

    feat: Refactor project controllers to implement ProjectCreationController and remove legacy controllers

--- server/controllers/project/index.js
+++ server/controllers/project/index.js
@@ -1,17 +1,12 @@
 // Project Controllers Module Export
 // Implements the modular controller layer from BACKEND_ARCHITECTURE_PLAN.md
 
-const projectController = require("./projectController");
-const repositoryController = require("./repositoryController");
-const settingsController = require("./settingsController");
+const projectCreationController = require('./projectCreationController');
 
 module.exports = {
-  project: projectController,
-  repository: repositoryController,
-  settings: settingsController,
+  // Project creation controller (new intelligent flow)
+  creation: projectCreationController,
 
-  // Flat exports for backward compatibility
-  ...projectController,
-  ...repositoryController,
-  ...settingsController,
+  // Export creation controller methods for direct access
+  ...projectCreationController,
 };
